6928ede32c73f1c9b060c54f1a990dc344a9f210
Pratap Neaupane <sereversion@gmail.com>
2020-05-11 14:12:20 +0545

    Can know login from user app

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
     'flight.apps.FlightConfig',
     'django.contrib.admin',
     'django.contrib.auth',
--- users/views.py
+++ users/views.py
@@ -12,3 +12,22 @@ def index(request):
         "user": request.user
     }
     return render(request, "users/user.html", context)
+
+
+def login_view(request):
+    username = request.POST["username"]
+    password = request.POST["password"]
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "users/login.html", {"message": "Invalid credential"})
+
+def logout_view(request):
+    print("Logout")
+
+
+
+
+
